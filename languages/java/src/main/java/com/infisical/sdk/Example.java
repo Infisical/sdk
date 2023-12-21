2c4230f0fb2049d23d1add9b6acc5aee17e05830
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2023-12-21 14:20:03 +0400

    Resolve Guardian warning

--- languages/java/src/main/java/com/infisical/sdk/Example.java
+++ languages/java/src/main/java/com/infisical/sdk/Example.java
@@ -6,8 +6,8 @@ public class Example {
 
     public static void Main(String[] args) {
         ClientSettings settings = new ClientSettings();
-        settings.setClientID("77719230-a0b6-4590-8fbd-376e8b0898a0");
-        settings.setClientSecret("746d3b218d7841aa0a9e24d127f585f0d4d99aa7154de5a95918b0b774dc63ff");
+        settings.setClientID("CLIENT_ID");
+        settings.setClientSecret("CLIENT_SECRET");
         settings.setSiteURL("http://localhost:8080");
 
         InfisicalClient client = new InfisicalClient(settings);
