41f970e293654ad3ef305ee9f691817f501644fc
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2023-12-24 15:56:32 +0400

    Attach to process env setting

--- crates/infisical/src/manager/secrets/list.rs
+++ crates/infisical/src/manager/secrets/list.rs
@@ -13,6 +13,7 @@ pub struct ListSecretsOptions {
     pub project_id: String,
     pub path: Option<String>,
 
+    pub attach_to_process_env: Option<bool>,
     pub include_imports: Option<bool>,
 }
 
