6684a226c67fc2858353f9b2ac707177dfe77fbe
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2023-12-30 14:02:15 +0400

    Update example.ts

--- languages/node/example.ts
+++ languages/node/example.ts
@@ -4,47 +4,62 @@ import { GetSecretOptions, InfisicalClient, LogLevel } from "./src";
 const randomStr = () => Date.now().toString(36);
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
-const client = new InfisicalClient({
-    clientId: "92e6dae7-38ab-485d-8625-945a4f72c899",
-    clientSecret: "082ca0e72bfb8391acb834a7471e52773fab90cb61a95d10764ade9327ef347e",
+const uncached_client = new InfisicalClient({
+    clientId: "CLIENT_ID",
+    clientSecret: "CLIENT_SECRET",
     siteUrl: "http://localhost:8080",
-    cacheTtl: 10,
-    logLevel: LogLevel.Debug
+    cacheTtl: 0
+});
+
+const cached_client = new InfisicalClient({
+    clientId: "CLIENT_ID",
+    clientSecret: "CLIENT_SECRET",
+    siteUrl: "http://localhost:8080",
+    cacheTtl: 30
 });
 
 (async () => {
-    const projectId = "6587ff06fe3abf0cb8bf1742";
+    /*const projectId = "6587ff06fe3abf0cb8bf1742";
     const environment = "dev";
 
-    await client.listSecrets({ projectId, environment, attachToProcessEnv: true });
-
-    console.log(process.env);
-
-    /*
-    while (true) {
-        const promises: any = [];
-        for (let i = 0; i < 1; i++) {
-            promises.push(runTests());
-        }
-
-        await Promise.all(promises);
-    }*/
-})();
-
-async function runTests() {
-    const secretNames = ["TEST", "TEST2", "TEST3", "TEST4", "TEST5", "TEST6"];
-    let promises: any = [];
-
-    for (let i = 0; i++ < 10; i++) {
-        const options: GetSecretOptions = {
-            environment: "dev",
-            projectId: "6587ff06fe3abf0cb8bf1742",
-
-            secretName: secretNames[Math.floor(Math.random() * secretNames.length)]
-        };
-
-        promises.push(await client.getSecret(options));
+    await uncached_client.getSecret({ projectId, environment, secretName: "TEST" });
+    await cached_client.getSecret({ projectId, environment, secretName: "TEST" });
+    
+    console.time("uncached");
+    for (let i = 0; i < 10; i++) {
+        await uncached_client.getSecret({ projectId, environment, secretName: "TEST" }).then(console.log);
     }
+    console.timeEnd("uncached");
 
-    await Promise.all(promises);
-}
+    console.time("cached");
+    for (let i = 0; i < 10; i++) {
+        await cached_client.getSecret({ projectId, environment, secretName: "TEST" });
+    }
+    console.timeEnd("cached");
+
+    process.exit(0);*/
+
+    const getOptions = {
+        projectId: "PROJECT_ID",
+        environment: "dev",
+        secretName: "TEST"
+    } as const;
+
+    const startSecret = await cached_client.getSecret(getOptions);
+
+    console.time("fetch secret cached");
+    await cached_client.getSecret(getOptions);
+    console.timeEnd("fetch secret cached");
+
+    // update the secret to remove from cache
+    /*await cached_client.updateSecret({
+        ...getOptions,
+        secretValue: randomStr()
+    });*/
+
+    console.time("fetch secret uncached");
+    await cached_client.getSecret(getOptions);
+    console.timeEnd("fetch secret uncached");
+
+    process.exit(0);
+})();
