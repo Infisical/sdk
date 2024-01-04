0d6d598a00111d9c2f7ed4fe2645564100179da2
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-01-04 05:34:14 +0400

    Cleaned up decryption to use ? operator

--- crates/infisical/src/manager/cryptography/decrypt_symmetric.rs
+++ crates/infisical/src/manager/cryptography/decrypt_symmetric.rs
@@ -30,39 +30,19 @@ pub struct DecryptSymmetricResponse {
 }
 
 pub fn decrypt_symmetric(input: &DecryptSymmetricOptions) -> Result<DecryptSymmetricResponse> {
-    let decoded_tag = b64_decode!(&input.tag);
-    let decoded_key = b64_decode!(&input.key);
-    let iv = b64_decode!(&input.iv);
-    let decoded_ciphertext = b64_decode!(&input.ciphertext);
-
-    if decoded_tag.is_err() {
-        return Err(Error::DecryptSymmetricKeyError {
-            message: decoded_tag.unwrap_err().to_string(),
-        });
-    }
-
-    if decoded_key.is_err() {
-        return Err(Error::DecryptSymmetricKeyError {
-            message: decoded_key.unwrap_err().to_string(),
-        });
-    }
-
-    if iv.is_err() {
-        return Err(Error::DecryptSymmetricKeyError {
-            message: iv.unwrap_err().to_string(),
-        });
-    }
-
-    if decoded_ciphertext.is_err() {
-        return Err(Error::DecryptSymmetricKeyError {
-            message: decoded_ciphertext.unwrap_err().to_string(),
-        });
-    }
-
-    let decoded_tag = decoded_tag.unwrap();
-    let decoded_key = decoded_key.unwrap();
-    let iv = iv.unwrap();
-    let mut decoded_ciphertext = decoded_ciphertext.unwrap();
+    let decoded_tag = b64_decode!(&input.tag).map_err(|e| Error::DecryptSymmetricKeyError {
+        message: e.to_string(),
+    })?;
+    let decoded_key = b64_decode!(&input.key).map_err(|e| Error::DecryptSymmetricKeyError {
+        message: e.to_string(),
+    })?;
+    let iv = b64_decode!(&input.iv).map_err(|e| Error::DecryptSymmetricKeyError {
+        message: e.to_string(),
+    })?;
+    let mut decoded_ciphertext =
+        b64_decode!(&input.ciphertext).map_err(|e| Error::DecryptSymmetricKeyError {
+            message: e.to_string(),
+        })?;
 
     // We modify the ciphertext a little bit here to remove the pre-existing tag, and append the tag that was provided as a parameter.
     //decoded_ciphertext.truncate(decoded_ciphertext.len() - 16);
@@ -70,16 +50,12 @@ pub fn decrypt_symmetric(input: &DecryptSymmetricOptions) -> Result<DecryptSymme
 
     let nonce = GenericArray::from_slice(&iv);
 
-    let cipher = Aes256Gcm::new_from_slice(&decoded_key);
-
-    if cipher.is_err() {
-        return Err(Error::DecryptSymmetricKeyError {
-            message: "Failed to create cipher.".to_string(),
-        });
-    }
+    let cipher =
+        Aes256Gcm::new_from_slice(&decoded_key).map_err(|e| Error::DecryptSymmetricKeyError {
+            message: e.to_string(),
+        })?;
 
     let plaintext_bytes = cipher
-        .unwrap()
         .decrypt(nonce, decoded_ciphertext.as_ref())
         .map_err(|e| Error::DecryptSymmetricKeyError {
             message: e.to_string(),
