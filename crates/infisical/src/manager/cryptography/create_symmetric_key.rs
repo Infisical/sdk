8518fcf91db2216c84826f576732224d20799037
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-01-04 04:26:28 +0400

    Fixed type issue

--- crates/infisical/src/manager/cryptography/create_symmetric_key.rs
+++ crates/infisical/src/manager/cryptography/create_symmetric_key.rs
@@ -1,6 +1,8 @@
 use crate::error::Result;
 use base64::engine::Engine;
 use rand::Rng;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
 
 macro_rules! b64_encode {
     ($key:expr) => {
@@ -8,11 +10,17 @@ macro_rules! b64_encode {
     };
 }
 
-pub fn create_symmetric_key() -> Result<String> {
+#[derive(Serialize, Deserialize, Debug, JsonSchema)]
+#[serde(rename_all = "camelCase")]
+pub struct CreateSymmetricKeyResponse {
+    pub key: String,
+}
+
+pub fn create_symmetric_key() -> Result<CreateSymmetricKeyResponse> {
     // Generate a 256-bit key (32 bytes * 8 bits/byte)
     let key: Vec<u8> = rand::thread_rng().gen::<[u8; 32]>().to_vec();
 
     let encoded_key = b64_encode!(key);
 
-    return Ok(encoded_key);
+    return Ok(CreateSymmetricKeyResponse { key: encoded_key });
 }
