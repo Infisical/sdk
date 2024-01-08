12d1eba744594ca2b05649d85f0805d9af39be71
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-01-08 17:16:38 +0400

    Add user agent to settings

--- crates/infisical/src/client/client_settings.rs
+++ crates/infisical/src/client/client_settings.rs
@@ -3,7 +3,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, JsonSchema)]
 #[serde(default, rename_all = "camelCase")]
-#[cfg_attr(feature = "mobile", derive(uniffi::Record))]
 pub struct ClientSettings {
     // These are optional because the access token can be set directly as well
     pub client_secret: Option<String>,
@@ -14,6 +13,7 @@ pub struct ClientSettings {
     pub site_url: Option<String>,
 
     pub cache_ttl: Option<u64>, // This controls how often the cache should refresh, default is 300 seconds
+    pub user_agent: Option<String>, // We use this to identity which SDK/language was used to make a request.
 }
 
 impl Default for ClientSettings {
@@ -24,6 +24,7 @@ impl Default for ClientSettings {
             access_token: None,
             site_url: None,
             cache_ttl: None,
+            user_agent: Some("infisical-unknown-sdk".to_string()),
         }
     }
 }
