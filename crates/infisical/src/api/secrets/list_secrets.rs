71ddccb6bedeb6ce4e26d71bdd35226002884012
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-01-15 14:57:01 +0400

    Fixed include imports

--- crates/infisical/src/api/secrets/list_secrets.rs
+++ crates/infisical/src/api/secrets/list_secrets.rs
@@ -1,9 +1,25 @@
 use crate::error::api_error_handler;
 use crate::helper::{build_base_request, build_url};
-use crate::manager::secrets::{ListSecretsOptions, ListSecretsResponse};
+use crate::manager::secrets::{ListSecretsOptions, ListSecretsResponse, Secret};
 use crate::{error::Result, Client};
 use log::debug;
 use reqwest::StatusCode;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub(crate) struct ListSecretsResponseImports {
+    secret_path: String,
+    folder_id: String,
+    environment: String,
+    secrets: Vec<Secret>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct ImportResponse {
+    pub(crate) imports: Vec<ListSecretsResponseImports>,
+    pub(crate) secrets: Vec<Secret>,
+}
 
 pub async fn list_secrets_request(
     client: &mut Client,
@@ -16,7 +32,7 @@ pub async fn list_secrets_request(
         "workspaceId": input.project_id,
 
         "secretPath": input.path.as_ref().unwrap_or(&"/".to_string()), // default is "/"
-        "include_imports": input.include_imports.as_ref().unwrap_or(&false), // default is false
+        "include_imports": input.include_imports.unwrap_or(false).to_string(),
 
     });
 
@@ -43,6 +59,24 @@ pub async fn list_secrets_request(
     let status = response.status();
 
     if status == StatusCode::OK {
+        if input.include_imports.unwrap_or(false) == true {
+            let response = response.json::<ImportResponse>().await?;
+
+            let mut secrets = response.secrets.clone();
+
+            for import in response.imports {
+                secrets.extend(import.secrets);
+            }
+
+            if input.attach_to_process_env.unwrap_or(false) == true {
+                for secret in secrets.clone() {
+                    std::env::set_var(secret.secret_key, secret.secret_value);
+                }
+            }
+
+            return Ok(ListSecretsResponse { secrets });
+        }
+
         let response = response.json::<ListSecretsResponse>().await?;
 
         if input.attach_to_process_env.unwrap_or(false) == true {
