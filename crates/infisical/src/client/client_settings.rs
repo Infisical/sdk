750ab9339c6ec166a30ad3d42902a981797d0184
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-05-17 16:06:29 +0200

    Feat: auth parameter

--- crates/infisical/src/client/client_settings.rs
+++ crates/infisical/src/client/client_settings.rs
@@ -1,21 +1,48 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+use super::auth_method_settings::Authentication;
+
 #[derive(Serialize, Deserialize, Debug, JsonSchema)]
 #[serde(default, rename_all = "camelCase")]
 pub struct ClientSettings {
     // These are optional because the access token can be set directly as well
+    #[deprecated]
+    #[schemars(
+        description = "**DEPRECATED**: The client secret field is deprecated. Please use the new auth object field instead."
+    )]
     pub client_secret: Option<String>,
+
+    #[deprecated]
+    #[schemars(
+        description = "**DEPRECATED**: The client secret field is deprecated. Please use the new auth object field instead."
+    )]
     pub client_id: Option<String>,
+
+    #[deprecated]
+    #[schemars(
+        description = "**DEPRECATED**: The access token field is deprecated. Please use the new auth object field instead."
+    )]
     pub access_token: Option<String>,
 
-    // Access token is optional because the user can also provide a machine token.
+    #[schemars(
+        description = "The URL of the site to connect to. Defaults to \"https://app.infisical.com\"."
+    )]
     pub site_url: Option<String>,
 
-    pub cache_ttl: Option<u64>, // This controls how often the cache should refresh, default is 300 seconds
+    #[schemars(
+        description = "cacheTTL controls how often the cache should refresh, default is 300 seconds. Set to 0 to disable the cache."
+    )]
+    pub cache_ttl: Option<u64>,
     pub user_agent: Option<String>, // We use this to identity which SDK/language was used to make a request.
+
+    #[schemars(
+        description = "Configure the authentication method to use.\n\nMake sure to only set one one method at a time to avoid conflicts and unexpected behavior."
+    )]
+    pub auth: Authentication,
 }
 
+#[allow(deprecated)]
 impl Default for ClientSettings {
     fn default() -> Self {
         Self {
@@ -24,6 +51,7 @@ impl Default for ClientSettings {
             access_token: None,
             site_url: None,
             cache_ttl: None,
+            auth: Authentication::default(),
             user_agent: Some("infisical-unknown-sdk".to_string()),
         }
     }
