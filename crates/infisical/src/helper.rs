8c5fab60240163e2e13e802d5bf9c44bc326c48f
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-05-17 16:06:38 +0200

    Update helper.rs

--- crates/infisical/src/helper.rs
+++ crates/infisical/src/helper.rs
@@ -1,5 +1,6 @@
 use crate::{
-    api::access_token::access_token_request,
+    api::universal_auth_login::universal_auth_login,
+    client::auth_method_settings::AuthMethod,
     error::{Error, Result},
     manager::secrets::Secret,
     Client,
@@ -7,16 +8,82 @@ use crate::{
 use log::debug;
 use reqwest;
 pub async fn handle_authentication(client: &mut Client) -> Result<()> {
-    if client.auth.access_token == None {
-        let res = access_token_request(client).await?;
+    if client.auth.access_token.is_some() {
+        return Ok(());
+    }
 
-        debug!("New MI token: {}", res.access_token);
+    let validation_result = client.auth.validate();
 
-        if res.access_token.len() > 0 {
-            client.set_access_token(res.access_token);
+    if validation_result.is_err() {
+        let err = Error::AuthSanitizationError {
+            message: validation_result
+                .err()
+                .unwrap_or("Unknown error in auth validation".to_string())
+                .to_string(),
+        };
+
+        return Err(err);
+    } else {
+        debug!("Auth validation passed");
+    }
+
+    let auth_method = validation_result.unwrap_or(AuthMethod::UniversalAuth);
+    let access_token;
+
+    match auth_method {
+        AuthMethod::UniversalAuth => {
+            debug!("Auth method is Universal Auth");
+            let result = universal_auth_login(client).await?;
+            access_token = result.access_token;
+        }
+        AuthMethod::GcpIdToken => {
+            debug!("Auth method is GCP ID Token!!!");
+            debug!(
+                "GCP Identity ID: {}",
+                client.auth.gcp_auth.as_ref().unwrap().identity_id
+            );
+            access_token = "NOT_IMPLEMENTED".to_string();
         }
     }
+
+    debug!("New access token token: {}", access_token);
+
+    if access_token.is_empty() {
+        debug!("No access token obtained");
+        return Err(Error::NoAccessTokenObtained);
+    }
+
+    debug!("Setting access token");
+    client.set_access_token(access_token);
     Ok(())
+}
+
+pub fn ensure_unique_secrets_by_key(secrets: &mut Vec<Secret>) {
+    let mut secret_map = std::collections::HashMap::new();
+
+    // Iterate over the secrets and insert them into the map
+    for secret in std::mem::take(secrets) {
+        secret_map.insert(secret.secret_key.clone(), secret);
+    }
+
+    // Clear the original vector and extend it with the unique secrets
+    secrets.clear();
+    secrets.extend(secret_map.into_iter().map(|(_, v)| v));
+}
+
+pub fn set_env_vars(should_attach_envs: bool, secrets: &Vec<Secret>) {
+    if !should_attach_envs {
+        return;
+    }
+
+    for secret in secrets {
+        // check if a env variable with the same name already exists, if it does, skip
+        if std::env::var(&secret.secret_key).is_ok() {
+            continue;
+        }
+
+        std::env::set_var(&secret.secret_key, &secret.secret_value);
+    }
 }
 
 pub fn build_base_request(
