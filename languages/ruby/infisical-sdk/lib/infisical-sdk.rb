6ad8d6be97cfe73bc149b26d7397e0419a487481
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-07-22 11:21:24 +0200

    Ruby sdk wrapper

---
+++ languages/ruby/infisical-sdk/lib/infisical-sdk.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'dry-types'
+
+require_relative 'schemas'
+require_relative 'extended_schemas/schemas'
+require_relative 'infisical_lib'
+require_relative 'infisical_error'
+require_relative 'command_runner'
+require_relative 'clients/secrets'
+require_relative 'clients/auth'
+require_relative 'clients/encryption'
+
+module InfisicalSDK
+  class InfisicalClient
+    attr_reader :infisical, :command_runner, :secrets, :auth, :encryption
+
+    def initialize(site_url = nil, cache_ttl = 300)
+      settings = ClientSettings.new(
+        # We preset these values or we'll get type validation errors (thanks Quicktype!)
+        access_token: nil,
+        client_secret: nil,
+        client_id: nil,
+        auth: nil,
+        user_agent: 'infisical-ruby-sdk',
+        cache_ttl: cache_ttl,
+        site_url: site_url
+      )
+
+
+      @infisical = InfisicalLib
+      @handle = @infisical.init(settings.to_dynamic.compact.to_json)
+      @command_runner = CommandRunner.new(@infisical, @handle)
+      @secrets = SecretsClient.new(@command_runner)
+      @auth = AuthClient.new(@command_runner)
+      @encryption = EncryptionClient.new(@command_runner)
+    end
+
+    def free_mem
+      @infisical.free_mem(@handle)
+    end
+  end
+end
