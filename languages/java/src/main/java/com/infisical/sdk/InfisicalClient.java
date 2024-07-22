ac41ee184e35c00a669f8a4acb4e13a85f826be3
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-07-22 15:07:27 +0200

    removed schema specific code for faster testing

--- languages/java/src/main/java/com/infisical/sdk/InfisicalClient.java
+++ languages/java/src/main/java/com/infisical/sdk/InfisicalClient.java
@@ -1,6 +1,6 @@
 package com.infisical.sdk;
 
-import com.infisical.sdk.schema.*;
+// import com.infisical.sdk.schema.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
@@ -12,175 +12,185 @@ import java.util.function.Function;
 
 public class InfisicalClient implements AutoCloseable {
 
-    private final Pointer client;
-    private final InfisicalLibrary library;
-    private final CommandRunner commandRunner;
-    private boolean isClientOpen;
+  // private final Pointer client;
+  // private final InfisicalLibrary library;
+  // private final CommandRunner commandRunner;
+  private boolean isClientOpen;
 
-    public InfisicalClient(ClientSettings settings) {
+  public InfisicalClient() {
 
-        settings.setUserAgent("infisical-java-sdk");
+    // settings.setUserAgent("infisical-java-sdk");
 
-        String libraryName = "infisical_c";
-        String arch = System.getProperty("os.arch");
-        String os = System.getProperty("os.name").toLowerCase();
+    // String libraryName = "infisical_c";
+    // String arch = System.getProperty("os.arch");
+    // String os = System.getProperty("os.name").toLowerCase();
 
-        if (os.contains("linux") && arch.equals("aarch64")) {
-          if (isMusl()) {
-              libraryName = "infisical_c_musl";
-          } else {
-              libraryName = "infisical_c_gnu";
-          }
-        } 
+    // if (os.contains("linux") && arch.equals("aarch64")) {
+    // if (isMusl()) {
+    // libraryName = "infisical_c_musl";
+    // } else {
+    // libraryName = "infisical_c_gnu";
+    // }
+    // }
 
-        library = Native.load(libraryName, InfisicalLibrary.class);
+    // library = Native.load(libraryName, InfisicalLibrary.class);
 
-        try {
-            client = library.init(Converter.ClientSettingsToJsonString(settings));
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+    // try {
+    // client = library.init(Converter.ClientSettingsToJsonString(settings));
+    // } catch (JsonProcessingException e) {
+    // throw new RuntimeException(e);
+    // }
 
-        commandRunner = new CommandRunner(library, client);
-        isClientOpen = true;
+    // // commandRunner = new CommandRunner(library, client);
+    isClientOpen = true;
+  }
+
+  @SuppressWarnings("deprecation")
+  private static boolean isMusl() {
+    try {
+      Process process = Runtime.getRuntime().exec("ldd --version");
+      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+      String line = reader.readLine();
+      return line != null && line.contains("musl");
+    } catch (IOException e) {
+      // If we can't determine, assume it's not musl
+      return false;
     }
+  }
 
-    @SuppressWarnings("deprecation")
-    private static boolean isMusl() {
-        try {
-            Process process = Runtime.getRuntime().exec("ldd --version");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-            String line = reader.readLine();
-            return line != null && line.contains("musl");
-        } catch (IOException e) {
-            // If we can't determine, assume it's not musl
-            return false;
-        }
+  public void getSecret() {
+    // Command command = new Command();
+    // command.setGetSecret(options);
+
+    // ResponseForGetSecretResponse response = commandRunner.runCommand(command,
+    // InfisicalClient.throwingFunctionWrapper(Converter::ResponseForGetSecretResponseFromJsonString));
+
+    // errorCheck(response.getSuccess(), response.getErrorMessage());
+
+    // return response.getData().getSecret();
+
+  }
+
+  public void listSecrets() {
+    // Command command = new Command();
+    // command.setListSecrets(options);
+
+    // ResponseForListSecretsResponse response = commandRunner.runCommand(command,
+    // InfisicalClient.throwingFunctionWrapper(Converter::ResponseForListSecretsResponseFromJsonString));
+
+    // errorCheck(response.getSuccess(), response.getErrorMessage());
+
+    // return response.getData().getSecrets();
+
+  }
+
+  public void createSecret() {
+    // Command command = new Command();
+    // command.setCreateSecret(options);
+
+    // ResponseForCreateSecretResponse response = commandRunner.runCommand(command,
+    // InfisicalClient.throwingFunctionWrapper(Converter::ResponseForCreateSecretResponseFromJsonString));
+
+    // errorCheck(response.getSuccess(), response.getErrorMessage());
+    // return response.getData().getSecret();
+
+  }
+
+  public void updateSecret() {
+    // Command command = new Command();
+    // command.setUpdateSecret(options);
+
+    // ResponseForUpdateSecretResponse response = commandRunner.runCommand(command,
+    // InfisicalClient.throwingFunctionWrapper(Converter::ResponseForUpdateSecretResponseFromJsonString));
+
+    // errorCheck(response.getSuccess(), response.getErrorMessage());
+    // return response.getData().getSecret();
+
+  }
+
+  public void deleteSecret() {
+    // Command command = new Command();
+    // command.setDeleteSecret(options);
+
+    // ResponseForDeleteSecretResponse response = commandRunner.runCommand(command,
+    // InfisicalClient.throwingFunctionWrapper(Converter::ResponseForDeleteSecretResponseFromJsonString));
+
+    // errorCheck(response.getSuccess(), response.getErrorMessage());
+    // return response.getData().getSecret();
+
+  }
+
+  public String createSymmetricKey() {
+    // ArbitraryOptions options = new ArbitraryOptions();
+    // options.setData("");
+
+    // Command command = new Command();
+    // command.setCreateSymmetricKey(options);
+
+    // ResponseForCreateSymmetricKeyResponse response =
+    // commandRunner.runCommand(command,
+    // InfisicalClient
+    // .throwingFunctionWrapper(Converter::ResponseForCreateSymmetricKeyResponseFromJsonString));
+
+    // errorCheck(response.getSuccess(), response.getErrorMessage());
+    // return response.getData().getKey();
+
+    return "symmetric-key";
+  }
+
+  public void encryptSymmetric() {
+    // Command command = new Command();
+    // command.setEncryptSymmetric(options);
+
+    // ResponseForEncryptSymmetricResponse response =
+    // commandRunner.runCommand(command,
+    // InfisicalClient
+    // .throwingFunctionWrapper(Converter::ResponseForEncryptSymmetricResponseFromJsonString));
+
+    // errorCheck(response.getSuccess(), response.getErrorMessage());
+    // return response.getData();
+
+  }
+
+  public void decryptSymmetric() {
+    // Command command = new Command();
+    // command.setDecryptSymmetric(options);
+
+    // ResponseForDecryptSymmetricResponse response =
+    // commandRunner.runCommand(command,
+    // InfisicalClient
+    // .throwingFunctionWrapper(Converter::ResponseForDecryptSymmetricResponseFromJsonString));
+
+    // errorCheck(response.getSuccess(), response.getErrorMessage());
+    // return response.getData().getDecrypted();
+
+  }
+
+  private void errorCheck(boolean success, String errorMessage) {
+    if (!success) {
+      if (errorMessage.length() > 0) {
+        throw new RuntimeException(errorMessage);
+      }
+
+      throw new RuntimeException("Command failed to execute with no error code");
     }
+  }
 
-    public GetSecretResponseSecret getSecret(GetSecretOptions options) {
-        Command command = new Command();
-        command.setGetSecret(options);
+  static <T, R> Function<T, R> throwingFunctionWrapper(ThrowingFunction<T, R, Exception> throwingFunction) {
 
-        ResponseForGetSecretResponse response = commandRunner.runCommand(command,
-                InfisicalClient.throwingFunctionWrapper(Converter::ResponseForGetSecretResponseFromJsonString));
+    return i -> {
+      try {
+        return throwingFunction.accept(i);
+      } catch (Exception ex) {
+        throw new RuntimeException("Response deserialization failed");
+      }
+    };
+  }
 
-        errorCheck(response.getSuccess(), response.getErrorMessage());
-
-        return response.getData().getSecret();
+  @Override
+  public void close() {
+    if (isClientOpen) {
     }
-
-    public SecretElement[] listSecrets(ListSecretsOptions options) {
-        Command command = new Command();
-        command.setListSecrets(options);
-
-        ResponseForListSecretsResponse response = commandRunner.runCommand(command,
-                InfisicalClient.throwingFunctionWrapper(Converter::ResponseForListSecretsResponseFromJsonString));
-
-        errorCheck(response.getSuccess(), response.getErrorMessage());
-
-        return response.getData().getSecrets();
-    }
-
-    public CreateSecretResponseSecret createSecret(CreateSecretOptions options) {
-        Command command = new Command();
-        command.setCreateSecret(options);
-
-        ResponseForCreateSecretResponse response = commandRunner.runCommand(command,
-                InfisicalClient.throwingFunctionWrapper(Converter::ResponseForCreateSecretResponseFromJsonString));
-
-        errorCheck(response.getSuccess(), response.getErrorMessage());
-        return response.getData().getSecret();
-    }
-
-    public UpdateSecretResponseSecret updateSecret(UpdateSecretOptions options) {
-        Command command = new Command();
-        command.setUpdateSecret(options);
-
-        ResponseForUpdateSecretResponse response = commandRunner.runCommand(command,
-                InfisicalClient.throwingFunctionWrapper(Converter::ResponseForUpdateSecretResponseFromJsonString));
-
-        errorCheck(response.getSuccess(), response.getErrorMessage());
-        return response.getData().getSecret();
-    }
-
-    public DeleteSecretResponseSecret deleteSecret(DeleteSecretOptions options) {
-        Command command = new Command();
-        command.setDeleteSecret(options);
-
-        ResponseForDeleteSecretResponse response = commandRunner.runCommand(command,
-                InfisicalClient.throwingFunctionWrapper(Converter::ResponseForDeleteSecretResponseFromJsonString));
-
-        errorCheck(response.getSuccess(), response.getErrorMessage());
-        return response.getData().getSecret();
-    }
-
-    public String createSymmetricKey() {
-        ArbitraryOptions options = new ArbitraryOptions();
-        options.setData("");
-
-        Command command = new Command();
-        command.setCreateSymmetricKey(options);
-
-        ResponseForCreateSymmetricKeyResponse response = commandRunner.runCommand(command,
-                InfisicalClient
-                        .throwingFunctionWrapper(Converter::ResponseForCreateSymmetricKeyResponseFromJsonString));
-
-        errorCheck(response.getSuccess(), response.getErrorMessage());
-        return response.getData().getKey();
-    }
-
-    public EncryptSymmetricResponse encryptSymmetric(EncryptSymmetricOptions options) {
-        Command command = new Command();
-        command.setEncryptSymmetric(options);
-
-        ResponseForEncryptSymmetricResponse response = commandRunner.runCommand(command,
-                InfisicalClient
-                        .throwingFunctionWrapper(Converter::ResponseForEncryptSymmetricResponseFromJsonString));
-
-        errorCheck(response.getSuccess(), response.getErrorMessage());
-        return response.getData();
-    }
-
-    public String decryptSymmetric(DecryptSymmetricOptions options) {
-        Command command = new Command();
-        command.setDecryptSymmetric(options);
-
-        ResponseForDecryptSymmetricResponse response = commandRunner.runCommand(command,
-                InfisicalClient
-                        .throwingFunctionWrapper(Converter::ResponseForDecryptSymmetricResponseFromJsonString));
-
-        errorCheck(response.getSuccess(), response.getErrorMessage());
-        return response.getData().getDecrypted();
-    }
-
-    private void errorCheck(boolean success, String errorMessage) {
-        if (!success) {
-            if (errorMessage.length() > 0) {
-                throw new RuntimeException(errorMessage);
-            }
-
-            throw new RuntimeException("Command failed to execute with no error code");
-        }
-    }
-
-    static <T, R> Function<T, R> throwingFunctionWrapper(ThrowingFunction<T, R, Exception> throwingFunction) {
-
-        return i -> {
-            try {
-                return throwingFunction.accept(i);
-            } catch (Exception ex) {
-                throw new RuntimeException("Response deserialization failed");
-            }
-        };
-    }
-
-    @Override
-    public void close() {
-        if (isClientOpen) {
-            library.free_mem(client);
-            isClientOpen = false;
-        }
-    }
+  }
 
 }
