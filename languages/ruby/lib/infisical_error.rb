ef6247ab1780903652ca26ac7ef526e55f555a5e
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-07-22 11:10:51 +0200

    Restructure

--- languages/ruby/lib/infisical_error.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module InfisicalSDK
-  class InfisicalError < StandardError
-    def initialize(message = 'Failed to get get response')
-      super(message)
-    end
-  end
-end
